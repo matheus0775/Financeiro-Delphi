51d7220fbafbb5004a5023bc80d8e98a4da0cb13
Matheus <matheus@celtasistemas.com.br>
2025-04-17 15:39:41 -0400

    Ajustes

--- Financeiro/Financeiro.dpr
+++ Financeiro/Financeiro.dpr
@@ -9,7 +9,11 @@ uses
   Cadastro.Produto in 'Cadastro.Produto.pas' {frmCadastroProduto},
   Venda in 'Venda.pas' {frmVenda},
   Pesquisa.Produto in 'Pesquisa.Produto.pas' {frmPesquisaProduto},
-  Model.Produto in 'Model.Produto.pas';
+  Model.Produto in 'Model.Produto.pas',
+  Pesquisa.Cliente in 'Pesquisa.Cliente.pas' {frmPesquisaCliente},
+  Model.Cliente in 'Model.Cliente.pas',
+  Model.Funcionario in 'Model.Funcionario.pas',
+  Pesquisa.Funcionario in 'Pesquisa.Funcionario.pas' {frmPesquisaFuncionario};
 
 {$R *.res}
 
@@ -19,5 +23,7 @@ begin
   Application.CreateForm(TMinhaConexao, MinhaConexao);
   Application.CreateForm(TfrmVenda, frmVenda);
   Application.CreateForm(TfrmPesquisaProduto, frmPesquisaProduto);
+  Application.CreateForm(TfrmPesquisaCliente, frmPesquisaCliente);
+  Application.CreateForm(TfrmPesquisaFuncionario, frmPesquisaFuncionario);
   Application.Run;
 end.
